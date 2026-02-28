2e1b029cdbbb5a6292488ca632771bb51faa64f4
udamsl2006 <udamsamudayakothalawala@gmail.com>
2026-02-28 13:43:37 +0530

    Refactor app.module.ts to include routing and components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,37 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { StudentTableComponent } from './components/student-table/student-table.component';
-import { TeacherTableComponent } from './components/teacher-table/teacher-table.component';
-import { AddNewTeacherComponent } from './components/add-new-teacher/add-new-teacher.component';
-import { AddNewStudentComponent } from './components/add-new-student/add-new-student.component';
-import { EditStudentComponent } from './components/edit-student/edit-student.component';
-import { EditTeacherComponent } from './components/edit-teacher/edit-teacher.component';
+import { TeachersComponent } from './teachers/teachers.component';
+import { StudentsComponent } from './students/students.component';
 
+const routes: Routes = [
+  { path: '', redirectTo: '/teachers', pathMatch: 'full' },
+  { path: 'teachers', component: TeachersComponent },
+  { path: 'students', component: StudentsComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    StudentTableComponent,
-    TeacherTableComponent,
-    AddNewTeacherComponent,
-    AddNewStudentComponent,
-    EditStudentComponent,
-    EditTeacherComponent
+    TeachersComponent,
+    StudentsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     HttpClientModule,
-    FontAwesomeModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
