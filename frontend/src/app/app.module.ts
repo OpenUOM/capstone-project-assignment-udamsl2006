585833c58ab23644d1b97a8d0af670bcf26b5b6d
udamsl2006 <udamsamudayakothalawala@gmail.com>
2026-02-28 19:40:11 +0530

    Update app.module.ts

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,30 +1,37 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { RouterModule, Routes } from '@angular/router';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TeachersComponent } from './teachers/teachers.component';
-import { StudentsComponent } from './students/students.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { StudentTableComponent } from './components/student-table/student-table.component';
+import { TeacherTableComponent } from './components/teacher-table/teacher-table.component';
+import { AddNewTeacherComponent } from './components/add-new-teacher/add-new-teacher.component';
+import { AddNewStudentComponent } from './components/add-new-student/add-new-student.component';
+import { EditStudentComponent } from './components/edit-student/edit-student.component';
+import { EditTeacherComponent } from './components/edit-teacher/edit-teacher.component';
 
-const routes: Routes = [
-  { path: '', redirectTo: '/teachers', pathMatch: 'full' },
-  { path: 'teachers', component: TeachersComponent },
-  { path: 'students', component: StudentsComponent }
-];
 
 @NgModule({
   declarations: [
     AppComponent,
-    TeachersComponent,
-    StudentsComponent
+    NavbarComponent,
+    StudentTableComponent,
+    TeacherTableComponent,
+    AddNewTeacherComponent,
+    AddNewStudentComponent,
+    EditStudentComponent,
+    EditTeacherComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     HttpClientModule,
-    FormsModule,
-    RouterModule.forRoot(routes)
+    FontAwesomeModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
