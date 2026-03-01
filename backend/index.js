a4e26823f106c578b1f2a73c35f2b7caa322ec5e
udamsl2006 <udamsamudayakothalawala@gmail.com>
2026-03-01 12:56:25 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,11 @@
-const server  = require ("./server.js");
+const server = require("./server.js");
 
 if(process.env.NODE_ENV === "test"){
-  server.listen(4401, () => {
-    console.log(
-      "Capstone Project Backend is running on http://localhost:4401"
-    );
+  server.listen(3000, () => {
+    console.log("Capstone Project Backend is running on http://localhost:3000");
   });
 }else{
-  server.listen(4401, () => {
-    console.log(
-      "Capstone Project Backend is running on http://localhost:4401"
-    );
+  server.listen(8080, () => {
+    console.log("Capstone Project Backend is running on http://localhost:8080");
   });
 }
