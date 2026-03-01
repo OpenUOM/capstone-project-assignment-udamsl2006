c3307d1f109f502aabcb729e25eebb4d3d1c49a6
udamsl2006 <udamsamudayakothalawala@gmail.com>
2026-03-01 13:09:28 +0530

    Update edit-teacher.component.ts

--- frontend/src/app/components/edit-teacher/edit-teacher.component.ts
+++ frontend/src/app/components/edit-teacher/edit-teacher.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { AppServiceService } from '../../app-service.service';
+import { Router, NavigationExtras } from '@angular/router';
+import {AppServiceService} from '../../app-service.service';
 
 @Component({
   selector: 'app-edit-teacher',
@@ -8,52 +8,37 @@ import { AppServiceService } from '../../app-service.service';
   styleUrls: ['./edit-teacher.component.css']
 })
 export class EditTeacherComponent implements OnInit {
-  teacherData: any;
-  teacherId: any;
 
-  constructor(private service: AppServiceService, private router: Router) {
-    // Capture the state immediately in the constructor
-    const navigation = this.router.getCurrentNavigation();
-    if (navigation && navigation.extras && navigation.extras.state) {
-      this.teacherId = navigation.extras.state.id;
-    }
-  }
+
+  teacherData: any;
+
+
+  constructor(private service : AppServiceService, private router: Router) { }
+
+  navigation = this.router.getCurrentNavigation();
 
   ngOnInit(): void {
-    if (this.teacherId) {
-      this.getTeacherData();
-    } else {
-      // If no ID (e.g. on page refresh), send user back to the list
-      this.router.navigate(['']); 
+    this.getTeacherData();
+  }
+
+  getTeacherData(){
+    let teacher = {
+      id : this.navigation.extras.state.id
     }
+    this.service.getOneTeacherData(teacher).subscribe((response)=>{
+      this.teacherData = response[0];
+    },(error)=>{
+      console.log('ERROR - ', error)
+    })
   }
 
-  getTeacherData() {
-    const payload = { id: this.teacherId };
-    this.service.getOneTeacherData(payload).subscribe(
-      (response) => {
-        // Assuming response is an array based on your original code
-        this.teacherData = Array.isArray(response) ? response[0] : response;
-      },
-      (error) => {
-        console.log('ERROR - ', error);
-      }
-    );
+  editTeacher(values){
+    values.id = this.navigation.extras.state.id;
+    this.service.editTeacher(values).subscribe((response)=>{
+      this.teacherData = response[0];
+    },(error)=>{
+      console.log('ERROR - ', error)
+    })
   }
 
-  editTeacher(formValues: any) {
-    // Merge the ID with the form values
-    const payload = { ...formValues, id: this.teacherId };
-    
-    this.service.editTeacher(payload).subscribe(
-      (response) => {
-        console.log('Update Successful');
-        // Navigate back to the table after successful edit
-        this.router.navigate(['']); 
-      },
-      (error) => {
-        console.log('ERROR - ', error);
-      }
-    );
-  }
 }
