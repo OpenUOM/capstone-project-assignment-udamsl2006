e0891c20dc87ac5df0d8986209df423476709d6a
udamsl2006 <udamsamudayakothalawala@gmail.com>
2026-03-01 13:08:16 +0530

    Update student-table.component.ts

--- frontend/src/app/components/student-table/student-table.component.ts
+++ frontend/src/app/components/student-table/student-table.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from "@angular/core";
 import { Router, NavigationExtras } from "@angular/router";
-import { faTrash, faPlus, faPenSquare } from "@fortawesome/free-solid-svg-icons";
+import {
+  faTrash,
+  faPlus,
+  faPenSquare,
+} from "@fortawesome/free-solid-svg-icons";
 import { AppServiceService } from "../../app-service.service";
-
 @Component({
   selector: "app-student-table",
   templateUrl: "./student-table.component.html",
@@ -12,8 +15,8 @@ export class StudentTableComponent implements OnInit {
   faTrash = faTrash;
   faPlus = faPlus;
   faPenSquare = faPenSquare;
-  studentData: any[] = [];
-  selected: string = "Students";
+  studentData: any;
+  selected: any;
 
   constructor(private service: AppServiceService, private router: Router) {}
 
@@ -25,43 +28,54 @@ export class StudentTableComponent implements OnInit {
     this.router.navigate(["addStudent"]);
   }
 
-  editStudent(id: any) {
+  editStudent(id) {
     const navigationExtras: NavigationExtras = {
-      state: { id: id },
+      state: {
+        id: id,
+      },
     };
     this.router.navigate(["editStudent"], navigationExtras);
   }
 
   getStudentData() {
-    this.selected = "Students";
     this.service.getStudentData().subscribe(
-      (response: any) => {
-        this.studentData = Array.isArray(response)
-          ? response.map((item: any) => [item])
-          : Object.keys(response).map((key) => response[key]);
+      (response) => {
+        this.studentData = Object.keys(response).map((key) => [response[key]]);
       },
-      (error) => console.log("ERROR - ", error)
+      (error) => {
+        console.log("ERROR - ", error);
+      }
     );
   }
 
-  search(value: string) {
-    if (!value.trim()) {
-      this.getStudentData();
-    } else {
-      this.service.searchStudent({ name: value.trim() }).subscribe(
-        (response: any) => {
-          this.studentData = Array.isArray(response)
-            ? response.map((item: any) => [item])
-            : Object.keys(response).map((key) => response[key]);
-        },
-        (error) => console.log("ERROR - ", error)
-      );
-    }
-  }
-
-  deleteStudent(itemid: any) {
-    this.service.deleteStudent({ id: itemid }).subscribe(() => {
+  deleteStudent(itemid) {
+    const student = {
+      id: itemid,
+    };
+    this.service.deleteStudent(student).subscribe((response) => {
       this.getStudentData();
     });
+  }
+
+  search(value: string) {
+    if (value.length <= 0 || !value.trim()) {
+      // If search is empty, reload all student data
+      this.getStudentData();
+    } else {
+      this.service.getStudentData().subscribe(
+        (response) => {
+          const allStudents = Object.keys(response).map((key) => response[key]);
+          const foundItems = allStudents.filter((student) => {
+            return student.name
+              .toLowerCase()
+              .includes(value.toLowerCase().trim());
+          });
+          this.studentData = foundItems;
+        },
+        (error) => {
+          console.log("ERROR - ", error);
+        }
+      );
+    }
   }
 }
